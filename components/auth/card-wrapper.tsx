3c00efe289d1b9e06599da622ed44b3482198521
Treetasayut Chumphom <facelockpass@gmail.com>
2025-07-13 21:14:02 +0700

    Remove image column from User model and add migration script

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { 
+import {
   Card,
   CardContent,
   CardDescription,
@@ -8,7 +8,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { MoveLeft } from "lucide-react"
 import { cn } from "@/lib/utils"
+import Link from "next/link"
 
 interface CardWrapperProps {
   children: React.ReactNode
@@ -22,8 +25,13 @@ export function CardWrapper({
   footerLabel
 }: CardWrapperProps) {
   return (
-    <Card className={cn("w-full max-w-full xs:max-w-md rounded-none xs:rounded-xl")}>
+    <Card className={cn("w-full max-w-full xs:max-w-md rounded-none xs:rounded-xl relative")}>
       <CardHeader>
+        <div className="absolute left-5 top-5">
+          <Link href='/'>
+            <Button variant='ghost' className="cursor-pointer"><MoveLeft /></Button>
+          </Link>
+        </div>
         <CardTitle className="text-center text-xl">
           {headerLabel}
         </CardTitle>

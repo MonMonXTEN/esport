61dd1d672709cd77af9087c1612844d34d4f25e7
Treetasayut Chumphom <168367174+MonMonXTEN@users.noreply.github.com>
2025-07-31 21:36:04 +0700

    fix: bug delete user

--- components/sidebar/FooterSidebar.tsx
+++ components/sidebar/FooterSidebar.tsx
@@ -25,13 +25,19 @@ import {
 } from "@/components/ui/avatar";
 import { signOut } from "next-auth/react";
 import useProfile from "@/hooks/use-profile";
+import { useEffect } from "react";
 
 const avatar = "https://github.com/shadcn.png"
 
 export default function FooterSidebar() {
   const { data, isLoading } = useProfile()
   const { isMobile } = useSidebar()
-  return (
+  useEffect(() => {
+    if (!isLoading && !data) {
+      signOut({ redirect: true, callbackUrl: "/" })
+    }
+  }, [isLoading, data])
+  if (isLoading) return (
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
@@ -41,13 +47,13 @@ export default function FooterSidebar() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
             >
               <Avatar className="h-8 w-8 rounded-full grayscale">
-                <AvatarImage src={avatar} alt={data?.username} />
+                <AvatarImage src={avatar}/>
                 <AvatarFallback><Loader2 className="mx-auto h-4 w-4 animate-spin" /></AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{isLoading ? "กำลังโหลด" : data.name}</span>
+                <span className="truncate font-medium">กำลังโหลด</span>
                 <span className="text-muted-foreground truncate text-xs">
-                  {data?.role}
+                  {/* isLoading */}
                 </span>
               </div>
               <EllipsisVertical className="ml-auto size-4" />
@@ -62,13 +68,13 @@ export default function FooterSidebar() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={avatar} alt={data?.username} />
+                  <AvatarImage src={avatar} />
                   <AvatarFallback><Loader2 className="mx-auto h-4 w-4 animate-spin" /></AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{data?.name}</span>
+                  <span className="truncate font-medium">กำลังโหลด</span>
                   <span className="text-muted-foreground truncate text-xs">
-                    @{data?.username}
+                    {/* isLoading */}
                   </span>
                 </div>
               </div>
@@ -76,7 +82,65 @@ export default function FooterSidebar() {
             <DropdownMenuSeparator />
             <DropdownMenuItem
               className="cursor-pointer"
-              onClick={() => signOut({redirectTo: '/'})}
+              onClick={() => signOut({ redirectTo: '/' })}
+            >
+              <LogOut />
+              ออกจากระบบ
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  )
+
+  if (!data) return <div>Unauthorized</div>
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
+            >
+              <Avatar className="h-8 w-8 rounded-full grayscale">
+                <AvatarImage src={avatar} alt={data.username} />
+                <AvatarFallback><Loader2 className="mx-auto h-4 w-4 animate-spin" /></AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{isLoading ? "กำลังโหลด" : data.name}</span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {!isLoading && data.role}
+                </span>
+              </div>
+              <EllipsisVertical className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={avatar} alt={data.username} />
+                  <AvatarFallback><Loader2 className="mx-auto h-4 w-4 animate-spin" /></AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{data.name}</span>
+                  <span className="text-muted-foreground truncate text-xs">
+                    @{data.username}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => signOut({ redirectTo: '/' })}
             >
               <LogOut />
               ออกจากระบบ

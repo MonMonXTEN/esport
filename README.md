f313a7444d93da6ca26663b451c74a0b752a10dd
Treetasayut Chumphom <168367174+MonMonXTEN@users.noreply.github.com>
2025-08-07 01:04:50 +0700

    Update README and seed.ts Add .env.example

--- README.md
+++ README.md
@@ -6,20 +6,24 @@ Esport
 ```bash
 npx auth secret
 ```
-และควรใส่ AUTH_URL ลงใน .env ด้วย
+(และควรใส่ AUTH_URL ลงใน .env ด้วยหากมีการ deploy)
 
 คำสั่งสร้าง prisma connect:
 ```bash
 npx prisma init --datasource-provider mysql
 ```
-หรือ
-DATABASE_URL="mysql://root:password@localhost:3306/mydatabase"
+หรือเพิ่ม DATABASE_URL="mysql://root:password@localhost:3306/mydatabase" ลงใน .env
 
 ทำการ Migrations ให้ตรงกับ Schema เดิม:
 ```bash
 npx prisma migrate deploy # สำหรับใช้งานจริง
+npx prisma generate
 ```
 or
 ```bash
 npx prisma migrate dev # สำหรับพัฒนา
+```
+
+```bash
+npx prisma db seed # เพิ่มข้อมูล admin & team 32
 ```
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,44 @@
 import db from "@/lib/db";
+import bcrypt from "bcryptjs";
+import fs from 'node:fs'
+import path from "node:path";
 
 async function main() {
-  await db.tournament.create({
-    data: {
-      name: "Rov Tournament 2025"
-    }
+  // seed user admin
+  const hashedPassword = await bcrypt.hash("1234", 10)
+  await db.user.upsert({
+    where: { username: "admin" },
+    update: { password: hashedPassword },
+    create: {
+      username: "admin",
+      password: hashedPassword,
+    },
   })
+  console.log('Seeded admin success!')
+
+  // seed teams
+  const teamsPath = path.join(__dirname, 'teams.json')
+  const teamsRaw = fs.readFileSync(teamsPath, 'utf-8')
+  const teamsFromJson = JSON.parse(teamsRaw) as { name: string }[]
+
+  const teams = teamsFromJson.map(team => ({
+    name: team.name,
+    status: true,
+  }))
+
+  await db.team.createMany({
+    data: teams,
+    skipDuplicates: true,
+  })
+
+  console.log('Seeded teams success!')
 }
 
 main()
-  .then(async () => {
-    await db.$disconnect()
-  })
-  .catch(async (e) => {
+  .catch(e => {
     console.error(e)
-    await db.$disconnect()
     process.exit(1)
+  })
+  .finally(async () => {
+    await db.$disconnect()
   })
